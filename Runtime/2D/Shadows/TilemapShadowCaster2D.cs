706de2523408521f0f955ae5a8036e749f1bf479
Gableb <gableb@hotmail.fr>
2020-03-17 03:41:40 -0400

    using system

--- Runtime/2D/Shadows/TilemapShadowCaster2D.cs
+++ Runtime/2D/Shadows/TilemapShadowCaster2D.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 namespace UnityEngine.Experimental.Rendering.Universal {
-    
+
     [ExecuteInEditMode]
     [DisallowMultipleComponent]
     [AddComponentMenu("Rendering/2D/Tilemap Shadow Caster 2D (Experimental)")]
